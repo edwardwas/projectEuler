dd78062d8301bdda49b9a3fadc7c8b22796afd85
edwardwas <ed@wastell.co.uk>
2014-04-12 01:27:40 +0100

    Made tiny change because I was a bit overprotecive. Now code runs v. slightly faster with the same result. Just like last time.

--- 135/master.hs
+++ 135/master.hs
@@ -14,7 +14,7 @@ genPairs n = concatMap (\v -> map (\u -> (v,u)) [1.. div n v] )  [ 1..n]
 
 run :: Int -> Int -> Int
 --Calculate all value of which there are m soultions below n
-run n m = length $ filter (\x -> length x == m) $ takeWhile (\x -> head x < n) $ groupedVals n
+run n m = length $ filter (\x -> length x == m) $  groupedVals n
         where groupedVals n = group $ sort $ map multPair $ filter validPair $ genPairs n
 
 main = putStrLn $ show $ run (10^6) 10
