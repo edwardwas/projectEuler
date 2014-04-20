b3f0a217f7dfa24aeb912b7194a9f2405666443e
edwardwas <ed@Wastell.co.uk>
2014-04-20 13:14:37 +0000

    Made 077 faster by making it do less prime checks and making primes a smaller list of numbers, as we only need to go upto 71. I'm trying some more clever tricks, becasue it still takes four minutes

--- 077/master.hs
+++ 077/master.hs
@@ -2,7 +2,7 @@ import Data.List
 import Data.List.Ordered (minus)
 import Data.Array
 
-primes :: Integral a => a -> [a]
+primes :: Int-> [Int]
 primes n = helper [2..n]
         where helper l
                 | p > intSqrt n = l
@@ -10,35 +10,30 @@ primes n = helper [2..n]
                 where p = head l
 		      intSqrt = floor.sqrt.fromIntegral
 
-p = primes (10^6)
+p = primes (10^2)
 
-primeTest :: Int -> Bool
+primeTest :: Int-> Bool
 primeTest x = elem x p
+primesLess :: Int -> [Int]
 primesLess x = takeWhile (<=x) p
 
+addToAll :: Ord c => [[c]] -> [[c]] -> [[c]]
+addToAll x y = [a ++ b | a <- x, b <- y ]
 
-addToAll :: [[a]] -> [[a]] -> [[a]]
-addToAll x y = [a ++ b | a <- x, b <- y]
+waysToSum :: Int -> [[Int]]
+waysToSum x = (listArray (2,100) $ map f [2..100]) ! x
+	where   f 2 = [[2]]
+		f 3 = [[3]]
+		f n = filterSets $ concatMap helper  $ primesLess (div n 2)
+			where filterSets = nub . map sort
+			      helper m  
+				| primeTest (n-m) =  addToAll ( [m] : waysToSum m) ([n-m] : waysToSum (n-m) )
+				| otherwise =  addToAll ( [m] : waysToSum m) (waysToSum (n-m) ) 
 
-setArr = listArray (2,100) $ map f [2..100]
-accArr x = setArr ! x
+primePartition :: Int -> Int
+primePartition n
+	| primeTest n = 1 + ans
+	| otherwise = ans
+	where ans = length $ waysToSum n
 
-f 2 = [[2]]
-f 3 = [[3]]
-f n = filterSets $ concatMap helper  $ primesLess (div n 2)
-	where helper m
-		| and [primeTest m, primeTest(n-m)] = addToAll ( [m] : accArr m) ([n-m] : accArr (n-m) )
-		| primeTest m = addToAll ( [m] : accArr m) (accArr (n-m) ) 
-		| primeTest (n-m) = addToAll ( accArr m) ([n-m] : accArr (n-m) ) 
-		| otherwise = addToAll (accArr m) (accArr (n-m) )
-
-priePart' n
-	| primeTest n = 1 + (length $ accArr n)
-	| otherwise = length $ accArr n
-
-
-
-filterSets :: Ord a => [[a]] -> [[a]]
-filterSets = nub . map sort 
-
-main = print $ show $ takeWhile (\(u,v) -> v <= 5000) $ zip [2..] $ map priePart' [2..]
+main = print $ show $ takeWhile (\(u,v) -> v <= 5000) $ zip [2..] $ map primePartition  [2..]
