df6a41d5fb14fc9e5ea5f90a64450d807ee30f5b
edwardwas <ed@Wastell.co.uk>
2014-05-04 22:17:57 +0000

    Correct slight mistake in output

--- 108/master.hs
+++ 108/master.hs
@@ -4,7 +4,7 @@ import Data.List
 divCountSqr :: Integral a => a -> Int
 divCountSqr = product . map (+1) . map ((*2) . length) . group . primeFactors 
 
-run :: Int -> Int
-run limit = head $ filter (2*limit<) $ map (divCountSqr)  [10..]
+run :: Int -> (Int,Int)
+run limit = head $ filter (\(u,v) -> v >2*limit) $ zip [10..] $ map  divCountSqr  [10..]
 
 main = print $ run (10^3)
