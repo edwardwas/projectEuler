19cc0d2e0a3a024ecadfad2b088da93fc0230c12
edwardwas <ed@Wastell.co.uk>
2014-05-04 22:16:39 +0000

    Made 108 look prettier

--- 108/master.hs
+++ 108/master.hs
@@ -1,11 +1,10 @@
 import Useful
 import Data.List
 
-
 divCountSqr :: Integral a => a -> Int
-divCountSqr n = product $ map (+1) $ map (\x -> 2* length x) $ group $ primeFactors n
+divCountSqr = product . map (+1) . map ((*2) . length) . group . primeFactors 
 
-run :: Int -> (Int,Int)
-run limit = head $ filter (\(u,v) -> v >2*limit) $ zip [10..] $ map (\n -> divCountSqr (n)) [10..]
+run :: Int -> Int
+run limit = head $ filter (2*limit<) $ map (divCountSqr)  [10..]
 
 main = print $ run (10^3)
