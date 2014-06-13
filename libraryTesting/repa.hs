75eeebe3d70812d2ab37d35f9cddd009ec11f9af
edwardwas <ed@wastell.co.uk>
2014-06-13 17:27:40 +0100

    Pruning

--- libraryTesting/repa.hs
+++
@@ -1,1 +0,0 @@
-import Data.Array.Repa as R
--- libraryTesting/vector.hs
+++
@@ -1,5 +0,0 @@
-import qualified Data.Vector as Vec
-
-numDigits = (+) 1 . floor . logBase 10 . fromIntegral
-
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
