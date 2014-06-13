1dafb1aa0117a6802fd91f17f1f95143284955ff
edwardwas <ed@wastell.co.uk>
2014-06-13 23:26:43 +0100

    Sped up 60 using new prime method

--- 060/master.hs
+++ 060/master.hs
@@ -2,15 +2,21 @@ import qualified Data.Numbers.Primes as Primes
 import Data.List.Ordered
 import Data.List
 import System.Environment
+import System.Random
+
+import Primes as OP
 
 isPrime :: Integer -> Integer -> Bool
-isPrime start x = member x $ dropWhile (< start) $ Primes.primes
+isPrime start x = fst $ OP.check x 5 $ mkStdGen 100
+isPrime' start x = member x $ dropWhile (< start) $ Primes.primes
 
 dig2num :: Integral a => [a] -> a
 dig2num = sum . zipWith(*) (iterate (*10) 1) 
 
 num2dig :: Integral a => a -> [a]
 num2dig = map (flip (mod) 10) . takeWhile (>0) . iterate (flip (div) 10)
+
+digSum = sum.num2dig
 
 catNum :: Integral a => a -> a -> [a]
 catNum x y = map dig2num $ [a++b,b++a ]
@@ -22,7 +28,9 @@ allCat x = concat $ helper x
 	where   helper (_:[]) = []
 		helper (x:xs) = (helper xs) ++ (map (catNum x) xs)
 
-pairWith pl x = filter (all (isPrime x) . allCat) $ [ [x,b] | b <- dropWhile (<= x) $ tail pl]
+pairWith pl x = filter (all (isPrime x) . allCat) 
+        $ filter (\y -> (mod (sum $ y) 3) /= 0)
+        $ [ [x,b] | b <- dropWhile (<= x) $ tail pl]
 
 addPair pl x = [ x ++ [b] | b <- map last $ pairWith pl $ last x 
 		, all (isPrime $ head x) $ allCat $ (init x) ++ [b]]
@@ -33,7 +41,7 @@ main = do
 	args <- getArgs
 	let len = read (head args) :: Int
 	let start = read (head $ tail args) :: Integer
-	print $ run (takeWhile (<1000) $ Primes.primes) len start
+	print $ run (delete 5 $ tail $ takeWhile (<1000) $ Primes.primes) len start
 
 
 
