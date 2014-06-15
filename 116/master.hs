4c0a7cc2cb40ac952e3b3cf5e898eddc758259e9
edwardwas <ed@Wastell.co.uk>
2014-06-15 10:40:34 +0000

    Fixed typo.

--- 116/master.hs
+++ 116/master.hs
@@ -1,7 +1,7 @@
 
-fittings rl tl
+fitings rl tl
         | tl > rl = 0
         | tl == rl = 1
-        | otherwise = sum $ map (\x -> 1 + (fittings (rl - tl - x) tl )) [0..(rl-tl)]
+        | otherwise = sum $ map (\x -> 1 + (fitings (rl - tl - x) tl )) [0..(rl-tl)]
 
 main = print $ sum $ map (fitings 50) [2,3,5]
