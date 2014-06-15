b58f304aa153a4df3594623208be34bd12551ffa
edwardwas <ed@Wastell.co.uk>
2014-06-15 11:17:54 +0000

    Finsih 116. Code runs in < 0.005s on desktop

--- 116/master.hs
+++ 116/master.hs
@@ -1,7 +1,19 @@
+import System.Environment
+import Data.Array
 
 fitings rl tl
         | tl > rl = 0
         | tl == rl = 1
-        | otherwise = sum $ map (\x -> 1 + (fitings (rl - tl - x) tl )) [0..(rl-tl)]
+        | otherwise = sum $ map ((+) 1 . flip (fitings) tl . (-) (rl - tl)) [0..(rl-tl)]
 
-main = print $ sum $ map (fitings 50) [2,3,5]
+fit rl tl = helper rl
+	where helper x
+		| x < tl = 0
+		| x == tl = 1
+		| otherwise = sum $ map ((+) 1 . (!) dp . (-) (x - tl)) [0..(x-tl)]
+		where dp = array (0,rl+1) [(y, helper y) | y <- [0..rl+1]]
+
+main = do
+	args <- getArgs
+	let n = read (head args) :: Integer
+	print $ sum $ map (fit n) [2,3,4]
