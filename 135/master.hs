96d2b7a1bed9e217d9b1dcc2b3cccbc145212602
edwardwas <ed@Wastell.co.uk>
2014-08-24 19:33:38 +0000

    Refactored 135 to make it go faster. Now runs in ~ 4.5s

--- 135/master.hs
+++ 135/master.hs
@@ -1,20 +1,14 @@
 import Data.List
 
 validPair :: (Int,Int) -> Bool
-validPair (u,v) = and [ 3*u > v,mod (u+v) 4 == 0, mod (3*u - v) 4 == 0]
-
-multPair :: (Int,Int) -> Int
-multPair (u,v) = u*v
-
-isDivisable :: Int -> Int -> Bool
-isDivisable n x = mod n x == 0
+validPair (u,v) = 3*u > v && mod (u+v) 4 == 0 && mod (3*u - v) 4 == 0
 
 genPairs :: Int -> [(Int,Int)]
-genPairs n = concatMap (\v -> map (\u -> (v,u)) [1.. div n v] )  [ 1..n]
+genPairs n = concatMap (\v -> map ((,) v) [1 .. div n v] )  [ 1..n]
 
 run :: Int -> Int -> Int
 --Calculate all value of which there are m soultions below n
-run n m = length $ filter (\x -> length x == m) $  groupedVals n
-        where groupedVals n = group $ sort $ map multPair $ filter validPair $ genPairs n
+run m = length . filter ((== m) . length) . group . sort . 
+		map (uncurry (*)) . filter validPair . genPairs 
 
-main = putStrLn $ show $ run (10^6) 10
+main = print $ run 10 $ 10^6
