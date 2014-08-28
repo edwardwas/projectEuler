d5a9049457413d2a147959813305856c58b8720e
edwardwas <ed@wastell.co.uk>
2014-08-28 12:33:58 +0100

    Rewrote some of the early solutions

--- Early/master.hs
+++ Early/master.hs
@@ -6,12 +6,12 @@ import System.IO
 import qualified Data.Map as Map
 
 sol1 = sum $ union [3,6..999] [5,10..999]
-sol2 = sum $ filter (\x -> mod x 2 == 0) $ takeWhile (<=4*10^6) fib
+sol2 = sum $ filter (even) $ takeWhile (<=4*10^6) fib
 sol3 = maximum $ primeFactors 600851475143
 sol4 = maximum $ filter (isPalindrome . digits) [x*y | x <- [100..999],y <- [100..999]]
-sol5 = product $ map (\(u,v) -> u^v) $ zip p b
+sol5 = product $ zipWith (^) p b
         where a = concatMap (group.sort.primeFactors) [2..20]
-              b = map (\y -> maximum $ map length $ filter (\x -> head x == y) a) p
+              b = map (\y -> maximum $ map length $ filter ((== y) . head) a) p
               p = primes 20
 sol6 = (sum [1..100]) ** 2 - sum (map (**2) [1..100])
 sol7 = last $ take 10001 $ primes (10^6)
@@ -20,7 +20,7 @@ sol8 = do
         let a = maximum $ map product $ breakToFive $ map digitToInt contents
         print a
 sol10 = sum $ primes (2*10^6)
-sol12 = head $ dropWhile (\x -> numDivisors x < 500) triNum
+sol12 = head $ dropWhile ((< 500) . numDivisors) triNum
 
 
 fib = 1 : helper [1,1,2]
@@ -33,7 +33,6 @@ breakToFive x = [x]
 
 intSqrt :: Integral a => a -> a
 intSqrt = floor . sqrt . fromIntegral
-
 
 primes :: Integral a => a -> [a]
 primes n = helper [2..n]
@@ -66,11 +65,10 @@ isInt :: RealFrac a => a -> Bool
 isInt x = floor x == ceiling x
 
 numDivisors :: Integral a => a -> Int
-numDivisors n = product $ map (+1) $ map length $ group $ primeFactors n
+numDivisors = product . map (+1) . map length . group . primeFactors
 
 triNum :: Integral a => [a]
-triNum = map (\n -> div (n * (n+1) ) 2) [1..]
-
+triNum = scanl (+) 1 [2..]
 
 collatzMem = listArray (1,n) $ map collatz [1..n]
         where n = (10^6)
